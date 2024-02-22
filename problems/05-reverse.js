7ef52624998e6c3586c57c9ee1982c4cbefd023e
Alex Bryan <tabryan1990@gmail.com>
2024-02-22 00:00:00 -0500

    instructor walkthrough complete for problems 05, 06

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -12,23 +12,24 @@ reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
 
-debugger
+// function reverse(str) {
+
+//   if(str === "") {
+//     return ""
+//   } else {
+//     return reverse(str.substring(1)) + str.charAt(0);
+//   }
+
+// }
+
 function reverse(str) {
 
-  if(str === "") {
-    return ""
-  } else {
-    return reverse(str.substring(1)) + str.charAt(0);
+  if ( str.length === 0 ) {
+    return str;
   }
 
+  return reverse(str.slice(1)) + str[0];
 }
-
-reverse("house"); // "esuoh"
-reverse("dog"); // "god"
-reverse("atom"); // "mota"
-reverse("q"); // "q"
-reverse("id"); // "di"
-reverse(""); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -14,9 +14,19 @@ addToTwelve([1]); // false
 
 
 function addToTwelve(arr) {
-  // Your code here 
+
+  if ( arr.length < 2 ) return false;
+  if ( arr[0] + arr[1] === 12 ) return true;
+
+  return addToTwelve(arr.slice(1));
+
 }
 
+addToTwelve([1, 3, 4, 7, 5]); // true
+addToTwelve([1, 3, 4, 7, 6]); // false
+addToTwelve([1, 11, 4, 7, 6]); // true
+addToTwelve([1, 12, 4, 7, 6]); // false
+addToTwelve([1]); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
